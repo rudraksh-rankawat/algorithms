1295bb36759128dc0a9453a37bc46a49f34fb8cb
rudrakshrankawat <rudraksh.23bcs10004@ms.sst.scaler.com>
2025-03-18 01:52:09 +0530

    best solution, amazinggg (did it in first go)

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -1,38 +1,32 @@
-#gotta do again in place
-
+#best solution
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
 
-        arr1 = []
-        for i in range(m):
-            arr1.append(nums1[i])
+        fill = len(nums1) - 1
+        a = m - 1
+        b = n - 1
 
-        a = b = x = 0
-        while a < len(arr1) and b < len(nums2):
-            if arr1[a] < nums2[b]:
-                nums1[x] = arr1[a]
-                a += 1
-            else: 
-                nums1[x] = nums2[b]
-                b += 1
-            x += 1
+        while fill >= 0:
 
-        if a >= len(arr1):
-            for i in range(b, len(nums2), 1):
-                nums1[x] = nums2[i]
-                x += 1
+            if a == -1 or b == -1:
+                break
 
-        if b >= len(nums2):
-            for i in range(a, len(arr1), 1):
-                nums1[x] = arr1[i]
-                x += 1
+            if nums1[a] > nums2[b]:
+                nums1[fill] = nums1[a]
+                a -= 1
+            else:
+                nums1[fill] = nums2[b]
+                b -= 1
+            fill -= 1
 
-        return nums1
-            
-
+        if a == -1:
+            while fill >= 0:
+                nums1[fill] = nums2[b]
+                fill -= 1
+                b -= 1
 
 
 
