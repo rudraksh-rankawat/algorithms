1b9870508f03ae9ed38aae78e9fe46e13ca15c38
rudrakshrankawat <rudraksh.23bcs10004@ms.sst.scaler.com>
2025-03-18 02:14:06 +0530

    took 12 mins, loved the performance

---
+++ 26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,14 @@
+#took 12 mins
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        last = 0
+        for i in range(len(nums)):
+            if nums[last] == nums[i]:
+                continue
+            last += 1
+            nums[last] = nums[i]
+
+        
+        return last + 1
